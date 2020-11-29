6c1a7fb84593d2cc5abb452d1c4825ad042ffc37
jlbuild <juliabuildbot@gmail.com>
2020-11-29 17:29:00 +0000

    Pixman_jll build 0.40.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -7,7 +7,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libpixman,
-        "lib/libpixman-1.0.dylib",
+        "lib/libpixman-1.0.40.0.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
